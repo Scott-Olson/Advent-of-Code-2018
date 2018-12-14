17f9579c6c980cbbb0db7c3fbd18b8b7531e3093
Scott Olson <scotteolson3@gmail.com>
2018-12-14 08:09:46 +0100

    day 14 started

---
+++ day_14.py
@@ -0,0 +1,14 @@
+input14 = [5,5,4,4,0,1]
+
+testInput = [3,7]
+
+numRounds = 9
+
+for trial in range(0, numRounds):
+	# number of rounds desired for accuracy
+	sums = sum(testInput)
+	sums.split()
+	testInput.append(sums)
+	for i in range(0, len(input14)):
+
+	
