53592e9aa7589506a43a9b469502ebd0d54bce2a
Scott Olson <scotteolson3@gmail.com>
2018-12-19 04:49:22 +0100

    worked on day 16 more

--- day_16.py
+++ day_16.py
@@ -1,43 +1,121 @@
-data = open('input5.txt').read().split('\n')
+import re
+from collections import *
+from itertools import *
+from math import *
+
+test = open('input16.txt').read().strip().split('\n\n\n')
+lines = test[0].strip().split('\n')
 
 
-def addr(R, a, regB){
-	result = 
-	return a + regB
-}
+def addr(R, a, b, c):
+	R[c] = R[a] + R[b]
 
-def addi(R, a, B){
-	return a + B
-}
 
-def mulr(R, a, regB){
-	return a * regB
-}
+def addi(R, a, b, c):
+	R[c] = int(R[a]) + b
 
-def muli(R, a, B){
-	return a * B
-}
 
-def banr(R, a, B){
-	
-}
+def mulr(R, a, b, c):
+	R[c] = R[a] * R[b]
 
-def bani(R, a, B){
-	
-}
 
-def borr(R, a, regB){
-	
-}
+def muli(R, a, b, c):
+	R[c] = int(R[a]) + b
 
-def bori(R, a, B){
-	
-}
 
-def setr(R, a, regB){
-	return a
-}
+def banr(R, a, b, c):
+	R[c] = R[a] & R[b]
 
-def seti(R, A, B){
-	return A
-}
+
+def bani(R, a, b, c):
+	R[c] = int(R[a]) & b 
+
+
+def borr(R, a, b, c):
+	R[c] = R[a] | R[b]
+
+
+def bori(R, a, b, c):
+	R[c] = int(R[a]) | b
+
+
+def setr(R, a, b, c):
+	R[c] = R[a]
+
+
+def seti(R, a, b, c):
+	R[c] = a
+
+
+def gtir(R, a, b, c):
+	if a > int(R[b]):
+		R[c] = 1
+	else: 
+		R[c] = 0
+
+
+def gtri(R, a, b, c):
+	if int(R[a]) > b:
+		R[c] = 1
+	else:
+		R[c] = 0
+
+
+def gtrr(R, a, b, c):
+	if R[a] > R[b]:
+		R[c] = 1
+	else:
+		R[c] = 0
+
+
+def eqir(R, a, b, c):
+	if a == int(R[b]):
+		R[c] = 1
+	else: 
+		R[c] = 0
+
+
+def eqri(R, a, b, c):
+	if int(R[a]) == b:
+		R[c] = 1
+	else:
+		R[c] = 0
+
+
+def eqrr(R, a, b, c):
+	if R[a] == R[b]:
+		R[c] = 1
+	else:
+		R[c] = 0
+
+
+inst = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtrr, eqir, eqri, eqrr]
+options = {}
+
+def runTests(R, a, b, c):
+	local = 0
+	for m in inst:
+		print(m)
+		if m(R, a, b, c):
+			print("worked somehow")
+		else:
+			print("no go")
+
+	return local
+
+
+count = 0
+new = []
+d = re.compile(r'\d+')
+for i in range(0, len(lines), 4):
+	reg = d.findall(lines[i])
+	# print(reg)
+	local = runTests(reg, int(reg[1]), int(reg[2]) ,int(reg[3]))
+
+
+	if local >= 3:
+		count += 1
+
+print(count)
+
+
